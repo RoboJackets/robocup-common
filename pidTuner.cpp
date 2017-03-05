b7dc5ccd333466f0cb3203a1a86e517d2f0cd023
Chachmu <mwoodward@gatech.edu>
2017-03-05 14:45:58 -0500

    run same pid value

--- pidTuner.cpp
+++ pidTuner.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <algorithm>
 
-
+//THIS VERSION OF THE CLASS IS FOR GRAPHING THE PID SPACE ONLY
 PidTuner::PidTuner(float ip, float ii, float id, float Sp, float Si, float Sd) {
     _initialPid = PidSet(ip,ii,id);
     _currentPid =  _initialPid;
@@ -11,15 +11,20 @@ PidTuner::PidTuner(float ip, float ii, float id, float Sp, float Si, float Sd) {
     _cycles = 0;
     _testNum = 0;
 
-    _pScale = Sp;
-    _iScale = Si;
-    _dScale = Sd;
+    _pScale = .5; //ONLY CHECK P VALUES
+    _iScale = 0;
+    _dScale = 0;
 
     _overScale = 1;
 
     _threshold = .5;
 
     _overScale = 2; //the step is divided by overscale each time it overshoots the target
+
+    for(float p = -10; p<=10.5; p+=_pScale){
+        _testSets.push_back(_initialPid);
+    }
+    std::cout<<"PID, Error"<<std::endl;
 }
 
 PidTuner::PidSet::PidSet(float ip, float ii, float id) {
@@ -30,15 +35,6 @@ PidTuner::PidSet::PidSet(float ip, float ii, float id) {
 }
 
 void PidTuner::startCycle() {
-    //if old test set is done, determine new values to test
-    if(_testNum==_testSets.size()) {
-        _testSets.clear();
-        _testSets.push_back(PidSet(_currentPid.p, _currentPid.i, _currentPid.d));
-        _testSets.push_back(PidSet(_currentPid.p + _pScale, _currentPid.i, _currentPid.d));
-        _testSets.push_back(PidSet(_currentPid.p, _currentPid.i + _iScale, _currentPid.d));
-        _testSets.push_back(PidSet(_currentPid.p, _currentPid.i, _currentPid.d + _dScale));
-        _testNum = 0;
-    }
     _currentPid = _testSets[_testNum];
 }
 
@@ -48,34 +44,13 @@ void PidTuner::run(float err) {
 
 //returns if pid needs more tuning, if it does, sets up next test points
 bool PidTuner::endCycle() {
+    if(_testNum + 1 == _testSets.size()){
+        std::cout<<"_______________________________________________________"<<std::endl;
+        return false;
+    }
     _testSets[_testNum] = _currentPid;
     _testNum += 1;
 
-    if(_testNum==_testSets.size()) {
-        //finished testing set, determine best pid, then check if we need more tests
-        _cycles += 1;
-
-
-        PidSet bestPid = _testSets[0];
-        bestPid = *(std::min_element(std::begin(_testSets),std::end(_testSets),
-            [] (PidSet const& p1, PidSet const& p2){ return p1.score < p2.score;}));
-
-
-        _pScale *= _testSets[0].score > _testSets[1].score ? 1 : -1/_overScale;
-        _iScale *= _testSets[0].score > _testSets[2].score ? 1 : -1/_overScale;
-        _dScale *= _testSets[0].score > _testSets[3].score ? 1 : -1/_overScale;
-
-        _currentPid = PidSet(_testSets[0].p + _pScale, _testSets[0].i + _iScale, _testSets[0].d + _dScale);
-
-        //make sure we have a prev score, then check how much the score improved by
-        if(_cycles > 1 && _prevScore - bestPid.score < _threshold) {
-            _currentPid = bestPid;
-            return false; //Finished tuning
-        } else {
-            _prevScore = bestPid.score;
-            return true; //keep testing
-        }
-    } else {
-        return true; //keep testing
-    }
+    std::cout<<_currentPid.score<<std::endl;
+    return true;
 }
