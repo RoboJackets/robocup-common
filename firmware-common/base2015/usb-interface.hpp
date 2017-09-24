f5125229f4f67ad1c61b2d709ca4f06abbe48be5
Evan Peterson <77evan@gmail.com>
2017-09-23 20:02:29 -0400

    removed some 2015s

--- firmware-common/base2015/usb-interface.hpp
+++ firmware-common/base2015/usb-interface.hpp
@@ -2,12 +2,12 @@
 
 #include <stdint.h>
 
-static const uint16_t RJ_BASE2015_VENDOR_ID = 0x524A;
-static const uint16_t RJ_BASE2015_PRODUCT_ID = 0x4253;
-static const uint16_t RJ_BASE2015_RELEASE = 0x0000;
+static const uint16_t RJ_BASE_VENDOR_ID = 0x524A;
+static const uint16_t RJ_BASE_PRODUCT_ID = 0x4253;
+static const uint16_t RJ_BASE_RELEASE = 0x0000;
 
 // USB "control transfer" commands recognized by the base station
-enum Base2015ControlCommand {
+enum BaseControlCommand {
     RadioWriteRegister = 1,
     RadioReadRegister,
     RadioStrobe,
