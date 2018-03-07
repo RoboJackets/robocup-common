7eb66faf1944dfe3d5faca9c9c6312beb94a8160
Jason Chan <jason27chan@gmail.com>
2018-03-07 02:03:26 +0000

    cookout

--- Field_Dimensions.cpp
+++ Field_Dimensions.cpp
@@ -1,7 +1,7 @@
 #include "Field_Dimensions.hpp"
 
 const Field_Dimensions Field_Dimensions::Single_Field_Dimensions(
-    6.050f, 4.050f, 0.250f, 0.010f, 0.700f, 0.180f, 0.160f, 1.000f , 2.000f,
+    6.050f, 4.050f, 0.250f, 0.010f, 0.700f, 0.180f, 0.160f, 1.000f, 2.000f,
     0.500f, 1.000f, 0.350f, 6.550f, 4.550f);
 
 const Field_Dimensions Field_Dimensions::Double_Field_Dimensions(
